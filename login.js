6027d25b68c5e4b8692322a30cf0f775a47ba440
klperf <Leonid_Kolesnykov@epam.com>
2024-11-30 17:56:26 +0200

    Index page

--- login.js
+++ login.js
@@ -1,23 +1,22 @@
-// login.js
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.getElementById('login-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    fetch('https://render-backend-0epw.onrender.com/login', {
+    fetch('http://localhost:3000/login', {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, password })
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
     })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            window.location.href = '/dashboard.html';
-        } else {
-            alert('Invalid username or password!');
-        }
-    })
-    .catch(error => console.error('Ошибка:', error));
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.success) {
+                localStorage.setItem('isLoggedIn', 'true');
+                alert('Вхід успішний!');
+                window.location.href = 'about.html'; // Redirect after login
+            } else {
+                alert('Невірний логін або пароль!');
+            }
+        });
 });
